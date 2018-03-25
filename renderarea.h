8951be274cc5582cad062e1bf63e787a9b99ee01
Fabricio Resende <fabriciomind@gmail.com>
2018-03-25 01:51:46 -0300

    Rendering Astroid

--- renderarea.h
+++ renderarea.h
@@ -2,12 +2,35 @@
 #define RENDERAREA_H
 
 #include <QWidget>
+#include <QColor>
 
 class RenderArea : public QWidget
 {
     Q_OBJECT
 public:
     explicit RenderArea(QWidget *parent = nullptr);
+
+    QSize minimumSizeHint() const Q_DECL_OVERRIDE;
+    QSize sizeHint() const Q_DECL_OVERRIDE;
+
+    enum ShapeType { Astroid, Cycloid, HuygensCycloid, HypoCycloid };
+
+    void setBackgroundColor (QColor color) { mBackgroundColor = color; } // setter
+    QColor backgroundColor () const { return mBackgroundColor; } // getter
+
+    void setShape (ShapeType shape) { mShape = shape; }
+    ShapeType shape () const { return mShape; }
+
+protected:
+    void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
+
+private:
+    QPointF compute_astroid(float t);
+
+private:
+    QColor mBackgroundColor;
+    QColor mShapeColor;
+    ShapeType mShape;
 
 signals:
 
