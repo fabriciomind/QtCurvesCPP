46897a355d6382926f4dee58becbe729dbe8f246
Fabricio Resende <fabriciomind@gmail.com>
2018-03-25 22:22:31 -0300

    We can now customize scale, interval length and step counter.

--- renderarea.cpp
+++ renderarea.cpp
@@ -157,7 +157,6 @@ void RenderArea::paintEvent(QPaintEvent *event)
         pixel.setX(point.x() * mScale + center.x());
         pixel.setY(point.y() * mScale + center.y());
 
-        //painter.drawPoint(pixel);
         painter.drawLine(pixel, prevPixel);
 
         prevPixel = pixel;
